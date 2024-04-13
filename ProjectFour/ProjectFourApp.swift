d59fd51099518e0418bcd129e8554b327846de8b
mterekho <156861905+mterekho@users.noreply.github.com>
2024-04-12 20:31:29 -0400

    Initial Commit

---
+++ ProjectFour/ProjectFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectFourApp.swift
+//  ProjectFour
+//
+//  Created by Mike Terekhov on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
